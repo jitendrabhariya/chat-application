b9e4c5caeee71d550489f6fbfa9cacb47c808b36
jitendrabhariya <bhariyajitendra@gmail.com>
2026-05-01 17:33:23 +0530

    Update Auth.controllers.js

--- Backend/controllers/Auth.controllers.js
+++ Backend/controllers/Auth.controllers.js
@@ -60,7 +60,11 @@
 
   export const logOut = async(req,res)=>{
     try {
-        res.clearCookie("token")
+        res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None"
+})
         return res.status(200).json({message:"logout successfully"})
         
     } catch (error) {
